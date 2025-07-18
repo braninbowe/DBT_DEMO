d6b5de5feb45755e2a1e66551822b7d2766dd538
branin@gmail.com <branin@gmail.com>
2025-07-18 21:49:33 +0000

    daily_weather model committed

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,23 +4,14 @@ select
      TO_TIMESTAMP(STARTED_AT) as STARTED_AT,
      DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED,
      HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED,
-     CASE
-        WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) IN ('Sat','Sun')
-        THEN 'WEEKEND'
-        ELSE 'BUSINESS DAY'
-     END AS DAY_TYPE,
+ 
+     {{day_type('STARTED_AT')}} AS DAY_TYPE,
 
-     CASE
-        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (12,1,2)
-        THEN 'WINTER'
-        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (3,4,5)
-        THEN 'SPRING'
-        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (6,7,8)
-        THEN 'SUMMER'
-        ELSE 'FALL'
-     END AS SEASON_OF_YEAR,
-     
+     {{get_season('STARTED_AT')}} AS SEASON_OF_YEAR,
+
      DAYNAME(TO_TIMESTAMP(STARTED_AT)) AS DAY_OF_WEEK
+
+
 from
 {{ source('demo', 'bike') }}
 where STARTED_AT != 'started_at'
